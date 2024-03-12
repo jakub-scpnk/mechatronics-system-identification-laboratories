f65c617026bc706704daf8bba76d6ae6198f336c
jakub-scpnk <jakubszczepaniak987@gmail.com>
2024-03-12 13:49:47 +0100

    Adding first part of third task

--- laboratory1/lab_1_task_3.m
+++ laboratory1/lab_1_task_3.m
@@ -1,22 +1,41 @@
+%% First step
+
 fs = 100; % sampling rate
 
 % Creating a signal with given duration
 tend = 1; % [s]
-t = 0:(1/fs):tend;
+t = 0:(1/fs):tend; % time vector
 
+% Amplitudes
 A1 = 10;
 A2 = 0.02;
+% Frequencies
 f1 = 7; % [Hz]
 f2 = 20; % [Hz]
 
+% Signal creation
 y1 = A1*sin(2*pi*f1*t) + A2*sin(2*pi*f2*t);
 
+% Calculating fft
 Y = fft(y1);
-N = length(Y);
-df = fs/N;
-fv = (0:N-1)*df/N;
-fv = 20*log10(fv);
+Y_mag = abs(Y(1:N/2 + 1)); % Extracting magnitudes of positive component up
+                           % to Nyquist frequency
+N = length(Y);             % Calculating length of signal
+df = fs/N;                 % Frequency resolution
 
-figure()
-plot( fv , abs(Y)/(N/2))
-yscale('log')
+fv = linspace(0, fs/2, N/2 + 1);  % Frequency vector (up to Nyquist freq)
+
+% Convert magnitude to dB (log scale)
+Y_fft_dB = 20 * log10(Y_mag);
+
+% Plot the spectrum on a logarithmic scale
+figure;
+semilogx(fv, Y_fft_dB); % semilogx - x axis has a log scale
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
+xlim([0,(fs/2)]);
+grid on;
+
+%% Second step
+
+y2 = y1 * hann(N); % Multiplying first signal by Hann window
