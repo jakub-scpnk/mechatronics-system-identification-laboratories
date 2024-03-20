74982cd70c717b8c75590b97a4cb999f065fea67
jakub-scpnk <jakubszczepaniak987@gmail.com>
2024-03-20 19:04:17 +0100

    Working on different pc

--- laboratory2/lab_2_task_3.m
+++ laboratory2/lab_2_task_3.m
@@ -2,7 +2,7 @@
 
 % data – data from file
 % fs – sampling feequency
-[data,fs] = audioread(['ktoto.wav']);
+[data,fs] = audioread('ktoto.wav');
 
 t = 0:1/fs:(353136 - 1)/fs;% time vector [s]
 
@@ -11,6 +11,7 @@ plot(t',data);
 xlabel('time [s]');
 xlim([0,length(t)/fs]);
 
+
 % Num – numerator of FIR filter
-%dataF(:,1) = filter(Num,1,squeeze(data(:,1)));
-%dataF(:,2) = filter(Num,1,squeeze(data(:,2)));
+dataF(:,1) = filter(Num,1,squeeze(data(:,1)));
+dataF(:,2) = filter(Num,1,squeeze(data(:,2)));
