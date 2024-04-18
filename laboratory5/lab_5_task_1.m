3fac2ff30509e031d68af2e8c8ac5ba09e800ef2
Jakub Szczepaniak <65018324+jakub-scpnk@users.noreply.github.com>
2024-04-18 02:10:36 +0200

    Almost finished

--- laboratory5/lab_5_task_1.m
+++ laboratory5/lab_5_task_1.m
@@ -1,4 +1,4 @@
-% author: Jakub Szczepaniak
+% authors: Jakub Szczepaniak & Aleksandra Michalak
 % date: 16.04.2024r
 
 %% Task 1.1
@@ -27,15 +27,15 @@ phase = zeros(size(omega));
 
 % Calculate frequency response manually
 for i = 1:length(omega)
-    s = 1j * omega(i); % Complex frequency
-    H = polyval(num, s) / polyval(den, s); % Transfer function at frequency s
-    mag(i) = abs(H); % Magnitude
-    phase(i) = angle(H) * (180 / pi); % Phase in degrees
+    s = 1j * omega(i);                     % Complex frequency
+    H = polyval(num, s) / polyval(den, s); % Transfer function at freq s
+    mag(i) = abs(H);                       % Magnitude
+    phase(i) = angle(H) * (180 / pi);      % Phase in degrees
 end
 
 % Plot the Bode plot
 figure;
-subplot(2,1,1);
+subplot(211);
 semilogx(omega, 20*log10(mag));
 grid on;
 xlabel('Frequency [Hz]');
@@ -43,7 +43,7 @@ ylabel('Magnitude [dB]');
 title('Magnitude Bode Plot');
 xlim([10^4 10^7]);
 
-subplot(2,1,2);
+subplot(212);
 semilogx(omega, phase);
 grid on;
 xlabel('Frequency [Hz]');
@@ -51,16 +51,20 @@ ylabel('Phase [deg]');
 title('Phase Bode Plot');
 xlim([10^4 10^7]);
 
+save_fig2png(gcf,[16 9], 'lab_5_task_1_fig_1');
+
 % Displaying bode plot with built in function
 figure;
 bode(SYS)
+save_fig2png(gcf,[16 9], 'lab_5_task_1_fig_2');
 
 % Calculating damping factor
 
 % Logarithmic Method
 impulse_response = out.y1;
 peak_amplitude = max(impulse_response);
-decay_rate = -20 / length(impulse_response); % Assuming the impulse response decays linearly
+decay_rate = -20 / length(impulse_response); % Assuming the impulse
+                                             % response decays linearly
 A_n = peak_amplitude;
 A_n1 = peak_amplitude * exp(decay_rate);
 zeta_log = -log(A_n/A_n1) / sqrt(4*pi^2 + log(A_n/A_n1)^2);
@@ -70,22 +74,50 @@ omega_d = omega(find(20*log10(mag) <= -3, 1));  % Frequency at -3dB point
 omega_1 = interp1(20*log10(mag), omega, -3);    % Bandwidth frequency
 zeta_half_power = 1 / sqrt(1 + (omega_1/(2*omega_d))^2);
 
-disp(['Dimensionless damping factor (logarithmic method): ', num2str(zeta_log)]);
-disp(['Dimensionless damping factor (half-power method): ', num2str(zeta_half_power)]);
+disp(['Dimensionless damping factor (logarithmic method): ', ...
+    num2str(zeta_log)]);
+disp(['Dimensionless damping factor (half-power method): ', ...
+    num2str(zeta_half_power)]);
 
 
 %% Task 1.3
 
+L = 4e-6;   % [H]
+C = 1e-6;   % [F]
+dt = 1/1e6; % [s]
 
 
+for R = [5, 10, 100] % [Ohm]
+    num = [L 0];       % transfer function numerator
+    den = [R*L*C L R]; % transfer function denominator
 
+    out=sim('lab_5_rlc_sim.slx'); % starting simulink model
+    SYS = tf(num,den);
 
+    % Displaying bode plot with built in function
+    figure('Name', ['Bode plot R =', num2str(R)])
+    bode(SYS)
+    save_fig2png(gcf,[16 9], ['lab_5_task_1_bode_r_', num2str(R)]);
 
+    % Calculating damping factor
 
+    % Logarithmic Method
+    impulse_response = out.y1;
+    peak_amplitude = max(impulse_response);
+    decay_rate = -20 / length(impulse_response); % Assuming the impulse 
+                                                 % response decays linearly
+    A_n = peak_amplitude;
+    A_n1 = peak_amplitude * exp(decay_rate);
+    zeta_log = -log(A_n/A_n1) / sqrt(4*pi^2 + log(A_n/A_n1)^2);
 
+    % Half-Power Method
+    omega_d = omega(find(20*log10(mag) <= -3, 1));% Frequency at -3dB point
+    omega_1 = interp1(20*log10(mag), omega, -3);  % Bandwidth frequency
+    zeta_half_power = 1 / sqrt(1 + (omega_1/(2*omega_d))^2);
 
+    fprintf('For resistance R = %d \n', R)
+    fprintf('\nDimensionless damping factor (log method): %.4f', zeta_log)
+    fprintf('\nDimensionless damping factor (half-power method): %.9f', ...
+        zeta_half_power);
 
-
-
-
-
+end
---
+++ laboratory5/lab_5_task_2.m
@@ -0,0 +1,45 @@
+% author: Jakub Szczepaniak
+% date: 16.04.2024r
+
+% note: The script requires about 45s to run, wait until MATLAB stop saying
+% 'busy' in the lower left corner.
+%% Task 2.1
+
+R = 10;     % [Ohm]
+L = 4e-6;   % [H]
+C = 1e-6;   % [F]
+dt = 1/1e6; % [s]
+
+% RLC transfer function numerator and denominator
+num = [L 0];
+den = [R*L*C L R];
+fig_num = 3; % Number of figure
+
+for freq = [70, 80, 90]
+    out = sim('lab_5_task_2_sim.slx');
+
+    figure('Name','System response')
+    plot(out.y1)
+
+    % --- figure details ---
+    xlabel('time [s]')
+    ylabel('amplitude [-]')
+    title(['System response for excitation with sine of frequency ', ...
+        num2str(freq)])
+    xlim([0,1])
+    grid on
+    save_fig2png(gcf,[16 9], ['lab_5_fig_', num2str(fig_num)])
+    fig_num = fig_num + 1;
+    % --- figure details ---
+end
+
+%% Task 2.2
+
+A = 1;
+freq =70;
+t = out.tout; % Same time vector as in simulation
+sine_wave = A*sin(2*pi*t*freq);
+
+%SYS_response = conv(H,sine_wave);
+
+%plot(SYS_response)
